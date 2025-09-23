f333eb656ad8fb10e7a8ecba9770ed3f6aafdd1e
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-23 13:24:29 -0300

    chore: adiciona loggers ao servidor

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
 
-export const app = fastify()
+export const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+})
